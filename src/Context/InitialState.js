077f9f641175a2318b9983a4f0d069201c7b29bd
Nahuel Lezcano <nlezcano733@gmail.com>
2021-12-19 02:07:44 -0300

    gestion de context

---
+++ src/Context/InitialState.js
@@ -0,0 +1,6 @@
+export const InitialState = {
+	hardware: [],
+	components: [],
+	software: [],
+	games: [],
+};
--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { ProductsContext } from '../Context/ProductsContext';
 import HomeCarousel from '../Components/Carousel';
 
 const Home = (props) => {
+  const { games } = useContext(ProductsContext);
+
+  console.log(games);
 
   return (
     <>
